1dd70f5e768a0b31ccecb686768e5ebbbb8df6f1
hansikareddy01 <rhansika108@gmail.com>
2026-06-19 18:33:02 +0530

    Update windowfunction.sql

--- windowfunction.sql
+++ windowfunction.sql
@@ -1,15 +1,15 @@
-//window function 
-//assigning numbers to orders
+--window function rownumber()
+--assigning numbers to orders
  select order_id,order_purchase_timestamp ,
 row_number() over(
  order by order_purchase_timestamp
 ) as rn from orders 
-//assigning number to orders for each state
+--assigning number to orders for each state for group so we use partition
 select customer_id, customer_state , row_number() over(
 partition by customer_state
 order by customer_id
 ) as rn from customers
-//rownumber windowfunction with cte
+--find latest order of each customer
 with cte as(
 select order_id,customer_id,order_purchase_timestamp ,row_number() over(
 partition by customer_id
